b64496a5b55f62fa1d082118b520714dbd192f1e
Victor-Alexandru <victor21cuciureanu@gmail.com>
2020-04-05 18:08:34 +0300

    change to images

--- noise_levels/lib/main.dart
+++ noise_levels/lib/main.dart
@@ -1,65 +1,26 @@
-import 'package:noise_meter/noise_meter.dart';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'dart:async';
+import 'package:image_picker/image_picker.dart';
+
 
 void main() {
-  runApp(new MyApp());
+  runApp(new ImagePickerExample());
+}
+class ImagePickerExample extends StatefulWidget {
+  @override
+  _ImagePickerExampleState createState() => _ImagePickerExampleState();
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => new _MyAppState();
-}
+class _ImagePickerExampleState extends State<ImagePickerExample> {
+  File _image;
 
-class _MyAppState extends State<MyApp> {
-  bool _isRecording = false;
-  StreamSubscription<NoiseReading> _noiseSubscription;
-  NoiseMeter _noiseMeter;
+  Future getImage() async {
+    var image = await ImagePicker.pickImage(source: ImageSource.gallery);
 
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  void onData(NoiseReading noiseReading) {
-    this.setState(() {
-      if (!this._isRecording) {
-        this._isRecording = true;
-      }
+    setState(() {
+      _image = image;
     });
-    print(noiseReading.toString());
-  }
-
-  void startRecorder() async {
-    print('***NEW RECORDING***');
-    try {
-      _noiseMeter = new NoiseMeter();
-      _noiseSubscription = _noiseMeter.noiseStream.listen(onData);
-    } on NoiseMeterException catch (exception) {
-      print("--------------------");
-      print(exception);
-      print("--------------------");
-
-    }
-  }
-
-  void stopRecorder() async {
-    try {
-      if (_noiseSubscription != null) {
-        _noiseSubscription.cancel();
-        _noiseSubscription = null;
-      }
-      this.setState(() {
-        this._isRecording = false;
-      });
-    } catch (err) {
-      print('stopRecorder error: $err');
-    }
-  }
-
-  void printWrapped(String text) {
-    final pattern = new RegExp('.{1,800}'); // 800 is the size of each chunk
-    pattern.allMatches(text).forEach((match) => print(match.group(0)));
   }
 
   @override
@@ -67,31 +28,20 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Noise Level Example"),
+          title: Text('Image Picker Example'),
         ),
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                "Noise Level",
-              ),
-              Text(
-                'MIC ${(_isRecording ? 'ON' : 'OFF')} \n(Check Flutter log for data)',
-                style: Theme.of(context).textTheme.display1,
-              ),
-            ],
-          ),
+          child: _image == null ? Text('No image selected.') : Image.file(_image),
         ),
         floatingActionButton: FloatingActionButton(
-            onPressed: () {
-              if (!this._isRecording) {
-                return this.startRecorder();
-              }
-              this.stopRecorder();
-            },
-            child: Icon(this._isRecording ? Icons.stop : Icons.mic)),
-      ),
+          onPressed: getImage,
+          tooltip: 'Pick Image',
+          child: Icon(Icons.add_a_photo),
+        ),
+      )
     );
+
   }
 }
+
+
